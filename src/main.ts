ff9ec8a65be9603fe2f8402b79df7a1916423764
Gourish Bhagat <60651018+Gourish14@users.noreply.github.com>
2026-09-11 01:11:18 +0530

    Add Angular bootstrap entry

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, { providers: [provideAnimationsAsync()] }).catch((err) => console.error(err));
